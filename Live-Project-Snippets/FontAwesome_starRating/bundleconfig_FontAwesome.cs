a50a98f378f6d567a07e5e209ec512b283a85b41
James Knorr <jknorr@coachmeapps.com>
2018-04-01 12:30:28 -0700

    upload snippets

---
+++ Live-Project-Snippets/FontAwesome_starRating/bundleconfig_FontAwesome.cs
@@ -0,0 +1,12 @@
+﻿// Added FontAwsome files to project and bundled them to other Content files	
+
+	
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/font-awesome").Include(
+                       "~/Content/font-awesome.css",
+                       "~/Content/font-awesome.min.css"));
+        }
+    }
+}
---
+++ Live-Project-Snippets/PopulateTodaysDateTime/PopulateDateTimeInController.cs
@@ -0,0 +1,27 @@
+﻿
+// GET: Reviews/Create
+public ActionResult Create()
+{
+    ViewBag.BuyerId = new SelectList(db.Buyers, "BuyerId");
+    Review review = new Review();
+    review.ReviewDate = DateTime.Now;
+    return View(review);
+}
+
+// POST: Reviews/Create
+// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+[HttpPost]
+[ValidateAntiForgeryToken]
+public ActionResult Create([Bind(Include = "ReviewID,ReviewDate,ProductRating,ReviewDetails,BuyerId")] Review review)
+{
+    try
+    {
+        if (ModelState.IsValid)
+        {
+            review.ReviewID = Guid.NewGuid();
+            db.Reviews.Add(review);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
